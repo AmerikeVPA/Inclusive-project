02e0540420cfbe3a05a24f0fb0c99b7a8c8e33c9
VicPina <vicpanzu7@gmail.com>
2022-03-11 16:32:09 -0600

    Intento de input system y docs

--- Inclusive project/Assets/Scripts/NInput/IPPlayerController.cs
+++ Inclusive project/Assets/Scripts/NInput/IPPlayerController.cs
@@ -23,6 +23,8 @@ public class IPPlayerController : MonoBehaviour
     // Private timeout deltatime
     private float _recallTO;
 
+    private const float treshold = 0.1f;
+
     private CharacterController playerController;
     private IPPlayerInputs ipInputs;
 
@@ -52,10 +54,12 @@ public class IPPlayerController : MonoBehaviour
 
         _recallTO = recallTimeout;
     }
-    private void Update()
+    private void Update() 
     {
-        
+        Interact();
+        Walk(); 
     }
+    private void LateUpdate() { Rotation(); }
     private void Interact()
     {
         if (interInRange && _intObjInRange!=null)
@@ -66,5 +70,29 @@ public class IPPlayerController : MonoBehaviour
                 Destroy(_intObjInRange);
             }
         }
+    }
+    private void Rotation()
+    {
+        if(ipInputs.rotation.sqrMagnitude >= treshold)
+        {
+            _rotateSpeed = ipInputs.rotation.x * rotationSpeed * Time.deltaTime;
+            // rotate the player left and right
+            transform.Rotate(Vector3.up * _rotateSpeed);
+        }
+    }
+    private void Walk()
+    {
+        // normalise input direction
+        Vector3 inputDirection = new Vector3(ipInputs.movement.x, 0.0f, ipInputs.movement.y).normalized;
+        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+        // if there is a move input rotate player when the player is moving
+        if (ipInputs.movement != Vector2.zero)
+        {
+            // move
+            inputDirection = transform.right * ipInputs.movement.x + transform.forward * ipInputs.movement.y;
+        }
+
+        // move the player
+        playerController.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, 0.0f, 0.0f) * Time.deltaTime);
     }
 }
